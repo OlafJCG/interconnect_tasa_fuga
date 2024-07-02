1fe8fe1940482fa2cc5297218a0a726c0ed9b80f
OLAF CRUZ <chucholaf@hotmail.com>
2024-07-01 21:48:03 -0600

    funcion para evaluar modelo

--- interconnect_churn_rate.py
+++ interconnect_churn_rate.py
@@ -9,6 +9,9 @@ import re
 
 from matplotlib import pyplot as plt
 
+from sklearn import metrics
+import numpy as np
+
 # %% [markdown]Funciones----------------------------------------------------------------------------------------------------------------------------------
 # ## Funciones
 # %%
@@ -30,6 +33,98 @@ def split_camel_to_snake(string, case='camel'):
         else:
             # Split dromedary case
             return '_'.join(re.findall(r'[A-Z]?[a-z]+|[A-Z]+(?=[A-Z]|$)', string)).lower()
+
+## Funcion para evaluar los modelos
+def evaluate_model(model, train_features, train_target, test_features, test_target):
+    
+    eval_stats = {}
+    
+    fig, axs = plt.subplots(1, 3, figsize=(20, 6)) 
+    
+    for type, features, target in (('train', train_features, train_target), ('test', test_features, test_target)):
+        
+        eval_stats[type] = {}
+    
+        pred_target = model.predict(features)
+        pred_proba = model.predict_proba(features)[:, 1]
+
+        eval_stats[type]['Accuracy'] = metrics.accuracy_score(target, pred_target)
+        eval_stats[type]['F1'] = metrics.f1_score(target, pred_target)
+
+        # F1
+        f1_thresholds = np.arange(0, 1.01, 0.05)
+        f1_scores = [metrics.f1_score(target, pred_proba>=threshold) for threshold in f1_thresholds]
+        
+        # ROC
+        fpr, tpr, roc_thresholds = metrics.roc_curve(target, pred_proba)
+        roc_auc = metrics.roc_auc_score(target, pred_proba)    
+        eval_stats[type]['ROC AUC'] = roc_auc
+
+        # PRC
+        precision, recall, pr_thresholds = metrics.precision_recall_curve(target, pred_proba)
+        aps = metrics.average_precision_score(target, pred_proba)
+        eval_stats[type]['APS'] = aps
+
+        if type == 'train':
+            color = 'blue'
+        else:
+            color = 'green'
+
+        # Valor F1
+        ax = axs[0]
+        max_f1_score_idx = np.argmax(f1_scores)
+        ax.plot(f1_thresholds, f1_scores, color=color, label=f'{type}, max={f1_scores[max_f1_score_idx]:.2f} @ {f1_thresholds[max_f1_score_idx]:.2f}')
+        # establecer cruces para algunos umbrales        
+        for threshold in (0.2, 0.4, 0.5, 0.6, 0.8):
+            closest_value_idx = np.argmin(np.abs(f1_thresholds-threshold))
+            marker_color = 'orange' if threshold != 0.5 else 'red'
+            ax.plot(f1_thresholds[closest_value_idx], f1_scores[closest_value_idx], color=marker_color, marker='X', markersize=7)
+        ax.set_xlim([-0.02, 1.02])    
+        ax.set_ylim([-0.02, 1.02])
+        ax.set_xlabel('threshold')
+        ax.set_ylabel('F1')
+        ax.legend(loc='lower center')
+        ax.set_title(f'Valor F1') 
+
+        # ROC
+        ax = axs[1]    
+        ax.plot(fpr, tpr, color=color, label=f'{type}, ROC AUC={roc_auc:.2f}')
+        # establecer cruces para algunos umbrales        
+        for threshold in (0.2, 0.4, 0.5, 0.6, 0.8):
+            closest_value_idx = np.argmin(np.abs(roc_thresholds-threshold))
+            marker_color = 'orange' if threshold != 0.5 else 'red'            
+            ax.plot(fpr[closest_value_idx], tpr[closest_value_idx], color=marker_color, marker='X', markersize=7)
+        ax.plot([0, 1], [0, 1], color='grey', linestyle='--')
+        ax.set_xlim([-0.02, 1.02])    
+        ax.set_ylim([-0.02, 1.02])
+        ax.set_xlabel('FPR')
+        ax.set_ylabel('TPR')
+        ax.legend(loc='lower center')        
+        ax.set_title(f'Curva ROC')
+        
+        # PRC
+        ax = axs[2]
+        ax.plot(recall, precision, color=color, label=f'{type}, AP={aps:.2f}')
+        # establecer cruces para algunos umbrales        
+        for threshold in (0.2, 0.4, 0.5, 0.6, 0.8):
+            closest_value_idx = np.argmin(np.abs(pr_thresholds-threshold))
+            marker_color = 'orange' if threshold != 0.5 else 'red'
+            ax.plot(recall[closest_value_idx], precision[closest_value_idx], color=marker_color, marker='X', markersize=7)
+        ax.set_xlim([-0.02, 1.02])    
+        ax.set_ylim([-0.02, 1.02])
+        ax.set_xlabel('recall')
+        ax.set_ylabel('precision')
+        ax.legend(loc='lower center')
+        ax.set_title(f'PRC') 
+
+    df_eval_stats = pd.DataFrame(eval_stats)
+    # df_eval_stats = df_eval_stats.reindex(index=    ('Exactitud', 'F1', 'ROC AUC', 'APS'))
+    df_eval_stats = df_eval_stats.rename(index={'Accuracy':'Exactitud'})
+    df_eval_stats = df_eval_stats.round(2)
+    
+    print(df_eval_stats)
+    
+    return
 
 # %% [markdown]--------------------------------------------------------------------------------------------------------
 # ## Carga de los datos.
